40443b56e28d049cee5e3fa9063ff7f50851a270
trumpcritic <trumpcritic@example.com>
2017-12-17 12:11:51 -0600

    staticly generated html

---
+++ forbidden_terms.php
@@ -0,0 +1,11 @@
+<?php
+
+        $terms = [
+                'evidence-based' => ['evidence', 'based'],
+                'science-based' => ['science', 'based'],
+                'fetus' => ['fetus'],
+                'diversity' => ['diversity'],
+                'entitlement' => ['entitlement'],
+                'transgender' => ['transgender'],
+                'vulnerable' => ['vulernable'],
+        ];
--- step3_grep_forbidden_words.php
+++ step3_grep_forbidden_words.php
@@ -1,18 +1,8 @@
 <?php
+	require_once('forbidden_terms.php');
 
 	//this simply greps the budget text files looking for generous combinations of each of the forbidden seven words.
 	//it will output into the grep_results sub folder
-	$terms = [
-		'evidence-based' => ['evidence', 'based'],
-		'science-based' => ['science', 'based'],
-		'fetus' => ['fetus'],
-		'diversity' => ['diversity'],
-		'entitlement' => ['entitlement'],
-		'transgender' => ['transgender'],
-		'vulnerable' => ['vulernable'],
-	];
-
-
 
 	foreach($terms as $term_name => $term_set){
 
